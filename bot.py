7651bc98636869cb5948ec3c0382147650fd567d
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2021-02-21 13:42:48 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN
+
+logging.basicConfig(level = logging.INFO)
+
+bot = Bot(token = TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler
+async def start(message:types.Message):
+  await message.reply("Hello, jopa ebanaya")
+
+@dp.message_handler()
+async def repeat(message:types.Message):
+  await message.answer(message.text)
+
+
+if __name__ == '__main__':
+  executor.start_polling(dp, skip_updates = True)
